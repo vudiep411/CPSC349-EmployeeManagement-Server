9f89783a70b045cadfc40b8bdfe074cf7ba2a3ea
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-03-30 14:57:29 -0700

    Changed cors settings

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000
 
 // App
 
-app.use(cors())
+app.use(cors({
+    origin: ["https://velvety-rabanadas-dffc6b.netlify.app"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}))
