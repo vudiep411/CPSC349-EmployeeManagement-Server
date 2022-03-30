358b6da2374a349c0fee0f350d06853acf2af98b
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-03-30 14:55:12 -0700

    Changed cors settings

--- server.js
+++ server.js
@@ -9,24 +9,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT || 5000
 
-const origin =
-  process.env.NODE_ENV === "production"
-    ? process.env.FRONTEND_PROD_URL
-    : process.env.FRONTEND_LOCAL_URL;
 
 // App
-app.use((req, res, next) => {
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
-app.use(cors({
-    origin: origin,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-}))
+
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}))
