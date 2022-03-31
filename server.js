1bea85d6728f997a225877a9416fecc06589ec55
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-03-31 15:49:51 -0700

    Changed cors settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
+const session = require('cookie-session')
 const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT || 5000
