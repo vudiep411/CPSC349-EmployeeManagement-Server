c5e818aa16ddf24aa2cd9de272fbb2a47a52d36a
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-03-31 15:41:02 -0700

    Changed cors settings

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 // Create session
+app.set('trust proxy', 1);
+
 app.use(session({
    key: "userId",
    secret: process.env.SESSION_SECRET,
@@ -34,35 +36,7 @@ app.use(session({
    }
 }))
 
-// Handle database disconnect
-const handleDisconnect = () =>
-{
-    db = mysql.createConnection({
-        user: process.env.user,
-        host: process.env.host,
-        password: process.env.password,
-        database: process.env.database
-    })
-    db.connect((err) => {
-        if(err)
-        {
-            console.log('Error when connecting to db:', err)
-            setTimeout(handleDisconnect, 2000)          
-        }
-    })
-    db.on('error', (err) => {
-        console.log('db error', err)
-        if(err.code === 'PROTOCOL_CONNECTION_LOST')
-        {
-            handleDisconnect();
-        }
-        else
-        {
-            throw err;
-        }
-    })
-}
-handleDisconnect()
+
 // Default Route
 app.use("/", require('./routes/controllers'))
 
